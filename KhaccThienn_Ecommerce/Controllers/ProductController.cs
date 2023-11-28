668a63f6766f6d1b566d9502e6ae081ea3026139
Lê Khắc Thiện <muzankibut112@gmail.com>
2023-11-29 01:57:43 +0700

    Feat: Update Logic Comment in Product Details

--- KhaccThienn_Ecommerce/Controllers/ProductController.cs
+++ KhaccThienn_Ecommerce/Controllers/ProductController.cs
@@ -1,9 +1,11 @@
-﻿using KhaccThienn_Ecommerce.Data;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using KhaccThienn_Ecommerce.Data;
 using KhaccThienn_Ecommerce.Models.DataModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Drawing;
 using X.PagedList;
 
 namespace KhaccThienn_Ecommerce.Controllers
@@ -11,9 +13,11 @@ namespace KhaccThienn_Ecommerce.Controllers
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ProductController(ApplicationDbContext context)
+        private readonly INotyfService _toastNotification;
+        public ProductController(ApplicationDbContext context, INotyfService toastNotification)
         {
             _context = context;
+            _toastNotification = toastNotification;
         }
         [HttpGet]
         public async Task<IActionResult> Index(string? name, string? sort, int? cateId, int page = 1, int limit = 9)
@@ -98,21 +102,64 @@ namespace KhaccThienn_Ecommerce.Controllers
             var userFound = await _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
 
             var product = await _context.Products.Include(c => c.Category).Include(x => x.Reviews).FirstOrDefaultAsync(x => x.Id == id);
+            var reviewByProd = await _context.Reviews.Include(r => r.User).Where(x => x.ProductId == id).OrderByDescending(x => x.Created_Date).ToListAsync();
             ViewBag.userFound = null;
             if (userFound != null)
             {
                 ViewBag.userFound = userFound;
                 Console.WriteLine($"UserID Header: {userFound?.Id}");
             }
-            return View(product);
+            ViewBag.product = product;
+            ViewBag.reviewByProd = reviewByProd;
+            return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult PostComment(int ProductId, int UserId, string Title, string Subject)
+        public async Task<IActionResult> PostComment(string previousUrl, [Bind("Id, Title, Subject, ProductId, UserId")] Review model)
         {
+            if (HttpContext.Session.GetInt32("userID") != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    model.Created_Date = DateTime.Now;
+                    _context.Add(model);
+                    await _context.SaveChangesAsync();
+                    _toastNotification.Success("Post Comment Success !", 2);
+                    return Redirect(previousUrl);
+                }
+                _toastNotification.Error("Post Comment Failed !", 2);
+                return Redirect(previousUrl);
+            }
+            else
+            {
+                //return RedirectToAction("Login", "User");
+                return RedirectToAction("Login", "User", new { returnUrl = previousUrl });
+            }
+        }
 
-            return Redirect(Request.GetDisplayUrl());
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteComment(string previousUrl, int Id)
+        {
+            if (HttpContext.Session.GetInt32("userID") != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    var cmtFound = await _context.Reviews.FirstOrDefaultAsync(x => x.Id == Id);
+                    _context.Reviews.Remove(cmtFound);
+                    await _context.SaveChangesAsync();
+                    _toastNotification.Success("Delete Comment Success !", 2);
+                    return Redirect(previousUrl);
+                }
+                _toastNotification.Error("Delete Comment Failed !", 2);
+                return Redirect(previousUrl);
+            }
+            else
+            {
+                //return RedirectToAction("Login", "User");
+                return RedirectToAction("Login", "User", new { returnUrl = previousUrl });
+            }
         }
     }
 }
